ef4957498fa421ad3828bf351abda4a5bdae6d6b
bend23 <andor.benedek23@gmail.com>
2025-04-02 20:47:40 +0300

    Venv check-some package install

--- BeeVolunteer/BeeVolunteer/settings.py
+++ BeeVolunteer/BeeVolunteer/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'BeeVolunteer',
     'pages',
-
 ]
 
 REST_FRAMEWORK = {
@@ -88,17 +87,17 @@ WSGI_APPLICATION = 'BeeVolunteer.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'mssql',  # Corrected ENGINE
-        'NAME': 'beeVolunteer',  # Should be a string
-        'USER': 'sa',
-        'PASSWORD': 'beeVolunteer1!',
-        'HOST': '127.0.0.1',  # or use the IP address if you prefer
-        'PORT': '1433',  # default port
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',  # ODBC driver name
-        },
-    }
+    # 'default': {
+    #     'ENGINE': 'mssql',  # Corrected ENGINE
+    #     'NAME': 'BeeVolunteer',  # Should be a string
+    #     'USER': 'sa',
+    #     'PASSWORD': 'beeVolunteer1!',
+    #     'HOST': '127.0.0.1',  # or use the IP address if you prefer
+    #     'PORT': '1433',  # default port
+    #     'OPTIONS': {
+    #         'driver': 'ODBC Driver 17 for SQL Server',  # ODBC driver name
+    #     },
+    # }
 }
 
 
